9941e989982e18ca9c51cb5662ea5e114730b4f5
Colm <colmdfahey@gmail.com>
2019-08-30 14:20:54 +0700

    060501

--- src/Components/Thumbnail.js
+++ src/Components/Thumbnail.js
@@ -3,16 +3,52 @@ import '../Styles/Thumbnail.css'
 class Thumbnail extends React.Component {
 
 	state = {
-    places: this.props.item
+    places: this.props.item,
+		liked: false
   }
+
+	likeUnlike(l) {
+		let likedStatus = l
+		if (l === false){
+			likedStatus = true
+			this.setState({
+				liked: likedStatus
+		})}
+		else {
+			likedStatus = true
+			this.setState(prevState => ({
+  		liked: !prevState.liked
+}))
+		}
+
+    }
+//is this the right way to do it? Found on stack overflow
+
+
+
+		//this method will change from false to true once. Won't change after that.
+
+
+	//I'm not using the stored value of places in state. Why am I saving to it?
+
+	//lots of different components could pass props to this component. Where are they saved/imported. Won't there be clashes when different components pass props with the same name?
+
 
 	render() {
 	  return (
 			<div className="thumbnail">
-				<p>Title: {this.props.item.title}</p><p>Price: €{this.props.item.price}</p><p>Location: {this.props.item.location}</p>
+				<p>Y{this.state.liked.toString()}</p>
+				<p>Title: {this.props.title}</p><p>Price: €{this.props.item.price}</p><p>Location: {this.props.item.location}</p>
+				<button onClick={() => this.likeUnlike(this.state.liked)}>Button</button>
 			</div>
 	  )
 	}
 }
+
+ // <button onClick={() => this.add(1,2)}>
+
+
+
+//i can use this.state.places.title instead of this.props.title. Are there advantages or disadvantages to using one over the other?
 
 export default Thumbnail
